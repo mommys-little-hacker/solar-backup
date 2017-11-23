076fa7725948bed011962352eba7328990cc9c14
Maxim Vasilev <m.vasilev@agima.ru>
2017-11-24 01:41:53 +0300

    mysqldump quickfix

--- backends/mysqldump.sh
+++ backends/mysqldump.sh
@@ -21,12 +21,12 @@ createDatabaseBackup() {
         mysqldump_opts="--single-transaction --skip-lock-tables"
     fi
 
-    if [[ ${mysqldump_user-""} != "" ]]; then mysqldump_user="${mysqldump_user/#/'-u'}"; fi
-    if [[ ${mysqldump_pass-""} != "" ]]; then mysqldump_pass="${mysqldump_pass/#/'-p'}"; fi
-    if [[ ${mysqldump_host-""} != "" ]]; then mysqldump_host="${mysqldump_host/#/'-h'}"; fi
+    if [[ ${mysqldump_user-""} != "" ]]; then _mysqldump_user="${mysqldump_user/#/'-u'}"; fi
+    if [[ ${mysqldump_pass-""} != "" ]]; then _mysqldump_pass="${mysqldump_pass/#/'-p'}"; fi
+    if [[ ${mysqldump_host-""} != "" ]]; then _mysqldump_host="${mysqldump_host/#/'-h'}"; fi
 
     mysqldump_cmd="mysqldump $mysqldump_opts
-        ${mysqldump_user-} ${mysqldump_pass-} ${mysqldump_host-} ${mysqldump_db-}"
+        ${_mysqldump_user-} ${_mysqldump_pass-} ${_mysqldump_host-} ${mysqldump_db-}"
 
     archive_name="mysql_${db}.${date_suffix}.sql.gz"
     source "backends/${files_backend}.sh"
