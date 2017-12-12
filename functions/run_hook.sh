124ce7bf927ed142ff0e4f9f6e5e993e0ec5c0ca
Maxim Vasilev <admin@qwertys.ru>
2017-12-12 17:55:50 +0300

    Fixed crash on absent hooks

--- functions/run_hook.sh
+++ functions/run_hook.sh
@@ -2,6 +2,8 @@ runHook () {
     hook=$1
     hook_path="${hooks_dir%%/}/${hook}"
 
+    if [[ "$hook" = "" ]]; then return 0; fi
+
     if [[ "$hooks_enable" = "true" && -x "$hook_path" ]]
     then
         logEvent "$MSG_HOOKS_START $hook"
