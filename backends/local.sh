bb456d676b6325c145ca95a2ea0a7fd63453fd9b
Maxim Vasilev <admin@qwertys.ru>
2017-12-12 17:25:50 +0300

    Added split functionality to backend local

--- backends/local.sh
+++ backends/local.sh
@@ -5,7 +5,8 @@
 # Common vars, must be present for every backend
 if [[ "${split_enabled-false}" = "true" ]]
 then
-    split -b${split_chunk_size-4G} - ${archive_name-backup} --filter='eval cat > ${local_basedir%%/}/$FILE'
+    stdin_conn="split -b${split_chunk_size-4G} - ${archive_name-backup} 
+        --filter='eval cat > ${local_basedir%%/}/$FILE'"
 else
     stdin_conn="eval cat > ${local_basedir%%/}/${archive_name-backup}"
 fi
