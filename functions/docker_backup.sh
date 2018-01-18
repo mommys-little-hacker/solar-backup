95aa754c5808dda365150c259cbc5d47b52a0010
Maxim Vasilev <admin@qwertys.ru>
2018-01-18 14:25:11 +0300

    Docker backup test release

--- functions/docker_backup.sh
+++ functions/docker_backup.sh
@@ -10,8 +10,32 @@ backupDocker() {
 
     runHook "${hooks_pre_docker-}" || return 1
 
-    echo
+    logEvent "$MSG_DOCKER_START"
+
+    for container in ${docker_cnts[@]}
+    do
+        tag="$date_suffix"
+        img="${container}:${tag}"
+        archive_name="docker_$container.$date_suffix.tar"
+
+        if [[ ! -r ${app_dir%%/}/backends/${files_backend}.sh ]]
+        then
+            logEvent "$MSG_NO_BACKEND"
+            return 1
+        fi
+
+        source "${app_dir%%/}/backends/${files_backend}.sh"
+        docker commit --pause=false $container ${container}:${tag} || { logEvent "$MSG_DOCKER_WARN"; continue ; }
+        docker save $img | $stdin_conn
+
+        pipestat="${PIPESTATUS[@]}"
+        if [[ ${pipestat[0]} != 0 && ${pipestat[1]} ]]
+        then
+            logEvent "$MSG_DOCKER_WARN"
+        fi
+    done
+
+    logEvent "$MSG_DOCKER_FINISH"
 
     runHook "${hooks_post_docker-}" || return 1
 }
-
